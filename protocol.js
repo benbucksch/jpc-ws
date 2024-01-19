6b477ce65ef2e59c8aed8e99f25803b6bf1cfbfb
Neil Rashbrook <neil.rashbrook@beonex.com>
2024-01-19 20:54:44 +0000

    Fix test

--- protocol.js
+++ protocol.js
@@ -2,7 +2,6 @@ import WSCall from "./WSCall.js";
 import JPCProtocol from "jpc-core/protocol.js";
 import WebSocketNode from "ws";
 import { assert } from "jpc-core/util.js";
-import { consoleError } from "../jpc-core/util.js";
 
 /**
  * Wire protocol API
@@ -115,22 +114,14 @@ export default class JPCWebSocket extends JPCProtocol {
    * Implements the wire protocol.
    *
    * @param method {string} the message name, e.g. "func", "get" etc.
-   * @param responseMethod {string} (optional)
-   *    if given, wait for the remote side to respond with this method,
-   *    and return the payload of `responseMethod`.
    * @param payload {JSON} see value in PROTOCOL.md
    * @returns {any} see value in PROTOCOL.md
-   *   The payload of the corresponding `responseMethod` answer.
-   *   If `responseMethod` is not given, returns null/undefined.
+   *   The payload of the corresponding answer.
    * @throws {Error} if:
    *   - the remote end threw an exception
    *   - the connection disappeared
    */
-  async callRemote(method, responseMethod, payload) {
-    if (responseMethod) {
-      return await this._wsCall.makeCall(method, payload);
-    } else {
-      return this._wsCall.makeCall(method, payload).catch(consoleError);
-    }
+  async callRemote(method, payload) {
+    return this._wsCall.makeCall(method, payload);
   }
 }
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,6 +1,6 @@
-import { expect, expectTypeOf, test, beforeAll, afterAll } from 'vitest';
+import { expect, test, beforeAll, afterAll } from 'vitest';
 import JPCWebSocket from "../protocol.js";
-import { start as startServer, kPort } from './server';
+import { start as startServer, stop as stopServer, kPort } from './server';
 
 let jpc;
 let app;
@@ -13,14 +13,14 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-  app.exit();
+  stopServer();
 });
 
 test('Car class', async () => {
   let cars = await app.cars;
   for (let car of cars) {
     let owner = await car.owner;
-    expect(owner).string();
+    expect(owner).toBeTypeOf('string');
     expect(await car.running).toBeFalsy();
     await car.startEngine();
     expect(await car.running).toBeTruthy();
